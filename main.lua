a81632f50b308665d42b4566892a36fc6cac7afe
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2020-11-29 16:22:49 -0300

    - criada interface inicial padrão do jogo

--- main.lua
+++ main.lua
@@ -7,6 +7,16 @@ local function construirJanela()
     love.window.setIcon(love.image.newImageData(ConstanteLove.IconeJogo))
 end
 
+local function definirPosicaoCentralJogadores(tamanhoRaquete)
+    local comprimento, largura = love.graphics.getDimensions()
+    return (comprimento / 2) - (tamanhoRaquete / 2), largura
+end
+
+local function definirPosicaoCentralBola(tamanhoBola)
+    local comprimento, largura = love.graphics.getDimensions()
+    return (comprimento / 2) - (tamanhoBola / 2), (largura / 2) - (tamanhoBola / 2)
+end
+
 function love.load()
     construirJanela()
 end
@@ -15,4 +25,11 @@ function love.update()
 end
 
 function love.draw()
+  local posicaoHorizontal, posicaoVertical = definirPosicaoCentralJogadores(ConstanteLove.comprimentoJogador)
+
+  love.graphics.rectangle("fill", posicaoHorizontal, 10, ConstanteLove.comprimentoJogador, ConstanteLove.alturaJogador)
+  love.graphics.rectangle("fill", posicaoHorizontal, posicaoVertical - 10, ConstanteLove.comprimentoJogador, ConstanteLove.alturaJogador)
+
+  local posicaoBolaX, posicaoBolaY = definirPosicaoCentralBola(ConstanteLove.tamanhoBola)
+  love.graphics.circle("fill", posicaoBolaX, posicaoBolaY, ConstanteLove.tamanhoBola)
 end
