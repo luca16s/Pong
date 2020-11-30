a0cd4666a742e0d11b6404de6150767b07ba9c81
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2020-11-30 12:49:17 -0300

    - incluido audio da bola na tabela de bola - alterada case das variaveis x e y da velociadade

--- main.lua
+++ main.lua
@@ -1,18 +1,18 @@
 local ConstanteLove = require 'ConstanteLove'
 
 local comprimento, largura = love.graphics.getDimensions()
-local audioBola
 local bola = {
     raio = ConstanteLove.raioBola,
     posicao = {
         X = (comprimento / 2) - (ConstanteLove.raioBola / 2),
-        Y = (largura / 2) - (ConstanteLove.raioBola / 2)
+        Y = (largura / 2) - (ConstanteLove.raioBola / 2),
     },
     velocidade = {
         base = 200,
-        X = 0,
-        Y = 0
+        X = 200 * math.cos(math.random() * 2 * math.pi),
+        Y = 200 * math.sin(math.random() * 2 * math.pi),
     },
+    audio = love.audio.newSource(ConstanteLove.somBola, "static"),
 }
 
 local function construirJanela()
@@ -26,29 +26,26 @@ local function definirPosicaoCentralJogadores(tamanhoRaquete)
 end
 
 local function movimentaBola(velocidade, bola)
-    bola.posicao.X = bola.posicao.X + bola.velocidade.x*velocidade
-    bola.posicao.Y = bola.posicao.Y + bola.velocidade.y*velocidade
+    bola.posicao.X = bola.posicao.X + bola.velocidade.X * velocidade
+    bola.posicao.Y = bola.posicao.Y + bola.velocidade.Y * velocidade
     if bola.posicao.X < bola.raio then
-      bola.velocidade.x = math.abs(bola.velocidade.x)
-      audioBola:stop() audioBola:play()
+      bola.velocidade.X = math.abs(bola.velocidade.X)
+      bola.audio:stop() bola.audio:play()
     elseif bola.posicao.X > comprimento-bola.raio then
-      bola.velocidade.x = -math.abs(bola.velocidade.x)
-      audioBola:stop() audioBola:play()
+      bola.velocidade.X = -math.abs(bola.velocidade.X)
+      bola.audio:stop() bola.audio:play()
     end
     if bola.posicao.Y < bola.raio then
-      bola.velocidade.y = math.abs(bola.velocidade.y)
-      audioBola:stop() audioBola:play()
+      bola.velocidade.Y = math.abs(bola.velocidade.Y)
+      bola.audio:stop() bola.audio:play()
     elseif bola.posicao.Y > largura-bola.raio then
-      bola.velocidade.y = -math.abs(bola.velocidade.y)
-      audioBola:stop() audioBola:play()
+      bola.velocidade.Y = -math.abs(bola.velocidade.Y)
+      bola.audio:stop() bola.audio:play()
     end
 end
 
 function love.load()
     construirJanela()
-    audioBola = love.audio.newSource(ConstanteLove.somBola, "static")
-    bola.velocidade.x = bola.velocidade.base * math.cos(math.random() * 2 * math.pi)
-    bola.velocidade.y = bola.velocidade.base * math.sin(math.random() * 2 * math.pi)
 end
 
 function love.update(dt)
