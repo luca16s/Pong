0f9a6f64cd78cd0543cf6551758d2f5feaffceca
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2020-12-02 19:27:29 -0300

    - inclusão do acender de luzes do nodemcu

--- pongNode.lua
+++ pongNode.lua
@@ -1,5 +1,4 @@
 local mqtt = require 'mqttNodeMCULibrary'
-local ConstanteNode = require 'ConstanteNode'
 
 local sensor = 0
 local ledJogador1 = 6
@@ -8,8 +7,19 @@ local botaoDireito = 1
 local botaoEsquerdo = 2
 local ultimaAcao = nil
 
+local hostServer = '192.168.1.2'
+local canalJogo = 'PONG_LUA_GAME'
+local comandoMoverEsquerda = '<PONG><MOVE><LEFT>'
+local comandoMoverDireita = '<PONG><MOVE><RIGHT>'
+local comandoParar = '<PONG><STOP>'
+local comandoVelocidadeBola = '<PONG><%f>'
+local comandoPontoJogador1 = '<PONG><LIGHT><GREEN>'
+local comandoPontoJogador2 = '<PONG><LIGHT><RED>'
+
 gpio.mode(ledJogador1, gpio.OUTPUT)
 gpio.mode(ledJogador2, gpio.OUTPUT)
+gpio.write(ledJogador1, gpio.LOW)
+gpio.write(ledJogador2, gpio.LOW)
 
 local function mensagemVelocidadeBola(mensagem, canal)
     mqtt.sendMessage(mensagem, canal)
@@ -19,9 +29,9 @@ local function mandaMensagemDireita(level)
     if level ~= ultimaAcao then
       ultimaAcao = level
       if level == 0 then
-        mqtt.sendMessage(ConstanteNode.comandoMoverDireita, ConstanteNode.canalJogo)
+        mqtt.sendMessage(comandoMoverDireita, canalJogo)
       elseif level == 1 then
-        mqtt.sendMessage(ConstanteNode.comandoParar, ConstanteNode.canalJogo)
+        mqtt.sendMessage(comandoParar, canalJogo)
       end
     end
   end
@@ -30,23 +40,25 @@ local function mandaMensagemDireita(level)
     if level ~= ultimaAcao then
       ultimaAcao = level
       if level == 0 then
-        mqtt.sendMessage(ConstanteNode.comandoMoverEsquerda, ConstanteNode.canalJogo)
+        mqtt.sendMessage(comandoMoverEsquerda, canalJogo)
       elseif level == 1 then
-        mqtt.sendMessage(ConstanteNode.comandoParar, ConstanteNode.canalJogo)
+        mqtt.sendMessage(comandoParar, canalJogo)
       end
     end
   end
 
 local function comandoRecebido(comando)
-  if comando == ConstanteNode.comandoMoverEsquerda or comando == ConstanteNode.comandoMoverDireita then
-    if comando == ConstanteNode.comandoPontoJogador1 then
+  if comando == comandoPontoJogador1 or comando == comandoPontoJogador2 then
+    if comando == comandoPontoJogador1 then
+      print('3')
       gpio.write(ledJogador2, gpio.LOW)
       gpio.write(ledJogador1, gpio.HIGH)
-    elseif comando == ConstanteNode.comandoPontoJogador2 then
-      gpio.write(ledJogador2, gpio.LOW)
-      gpio.write(ledJogador1, gpio.HIGH)
+    elseif comando == comandoPontoJogador2 then
+      print('4')
+      gpio.write(ledJogador1, gpio.LOW)
+      gpio.write(ledJogador2, gpio.HIGH)
     end
-    mensagemVelocidadeBola(string.format(ConstanteNode.comandoVelocidadeBola, adc.read(sensor)), ConstanteNode.canalBola)
+    mensagemVelocidadeBola(string.format(comandoVelocidadeBola, adc.read(sensor)), canalJogo)
   end
 end
 
@@ -55,4 +67,4 @@ gpio.mode(botaoEsquerdo, gpio.INPUT, gpio.PULLUP)
 gpio.trig(botaoDireito, 'both', mandaMensagemDireita)
 gpio.trig(botaoEsquerdo, 'both', mandaMensagemEsquerda)
 
-mqtt.start(ConstanteNode.hostServer, 'Paulo', ConstanteNode.canalJogo, comandoRecebido)
+mqtt.start(hostServer, 'Paulo', canalJogo, comandoRecebido)
