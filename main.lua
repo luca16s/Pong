c9a898826e81bb323c99acad37597ea6d04f6da5
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2020-12-08 21:43:01 -0300

    - melhorias de código para legibilidade

--- main.lua
+++ main.lua
@@ -1,44 +1,60 @@
----------------------------------------------
 local ConstanteLove = require 'ConstanteLove'
-local mqttLove = require 'mqttLoveLibrary'
-local comprimento, largura = love.graphics.getDimensions()
-local ParaJogo=true
-local FinalPartida=false
-local placarP1=0
-local placarP2=0
-mensagem=false
+local MqttServer = require 'mqttLoveLibrary'
+local comprimentoJanela, larguraJanela = love.graphics.getDimensions()
 math.randomseed(os.time())
----------------------------------------------
-function defineCos()
-  local angulosCos=math.random(55,90)
-  local criterio=math.random(0,9)
-  if criterio%2==1 then
-    angulosCos=-angulosCos
+
+local function defineAngulo()
+  local angulo = math.random(55, 90)
+  local criterio = math.random(0, 9)
+
+  if criterio%2 == 1 then
+    angulo = -angulo
   end
-  return angulosCos
+  return angulo
 end
-function defineSen()
-  local angulosSen=math.random(55,90)
-  local criterio=math.random(0,9)
-  if criterio%2==1 then
-    angulosSen=-angulosSen
-  end
-  return angulosSen
-end
---
+
+local jogo = {
+  fonte = love.graphics.newFont(ConstanteLove.nomeFonte, ConstanteLove.tamanhoFonte),
+  seno = defineAngulo,
+  cosseno = defineAngulo,
+  mostrarMensagemInicial = true,
+  partidaTerminada = false,
+  encerraPartida = true
+}
+
 local bola = {
+    --imagem = love.graphics.newImage(ConstanteLove.imagemBola),
     raio = ConstanteLove.raioBola,
     posicao = {
-        X = (comprimento / 2) - (ConstanteLove.raioBola / 2),
-        Y = (largura / 2) - (ConstanteLove.raioBola / 2),
+        X = (comprimentoJanela / 2) - (ConstanteLove.raioBola / 2),
+        Y = (larguraJanela / 2) - (ConstanteLove.raioBola / 2),
     },
     velocidade = {
         X = 10,
         Y = -200,
     },
-    audio = love.audio.newSource(ConstanteLove.somBola, "static"),
+    audio = love.audio.newSource(ConstanteLove.somBola, 'static'),
 }
-  
+
+local posicaoHorizontal = (comprimentoJanela / 2) - (ConstanteLove.comprimentoJogador / 2)
+
+local Player1 = {
+  --imagem = love.graphics.newImage(ConstanteLove.imagemJogador),
+  x = posicaoHorizontal,
+  y = 10,
+  largura = ConstanteLove.comprimentoJogador,
+  altura = ConstanteLove.alturaJogador,
+  placar = 0
+}
+
+local Player2 = {
+  --imagem = love.graphics.newImage(ConstanteLove.imagemJogador),
+  x = posicaoHorizontal,
+  y = larguraJanela - 20,
+  largura = ConstanteLove.comprimentoJogador,
+  altura = ConstanteLove.alturaJogador,
+  placar = 0
+}
 
 local function construirJanela()
     love.window.setFullscreen(false)
@@ -46,119 +62,141 @@ local function construirJanela()
     love.window.setIcon(love.image.newImageData(ConstanteLove.IconeJogo))
 end
 
-local function definirPosicaoCentralJogadores(tamanhoRaquete)
-    return (comprimento / 2) - (tamanhoRaquete / 2), largura
-end
-posicaoHorizontal, posicaoVertical = definirPosicaoCentralJogadores(ConstanteLove.comprimentoJogador)
-local P1={x=posicaoHorizontal,y=10,largura=ConstanteLove.comprimentoJogador,altura=ConstanteLove.alturaJogador,placar=placarP1}
-local P2={x=posicaoHorizontal,y=posicaoVertical - 20,largura=ConstanteLove.comprimentoJogador,altura=ConstanteLove.alturaJogador,placar=placarP2}
-
 local function movimentaBola(velocidade, bola)
     bola.posicao.X = bola.posicao.X + bola.velocidade.X * velocidade
     bola.posicao.Y = bola.posicao.Y + bola.velocidade.Y * velocidade
+
     if bola.posicao.X < bola.raio then
       bola.velocidade.X = math.abs(bola.velocidade.X)
       bola.audio:stop() bola.audio:play()
-    elseif bola.posicao.X > comprimento-bola.raio then
+    elseif bola.posicao.X > comprimentoJanela-bola.raio then
       bola.velocidade.X = -math.abs(bola.velocidade.X)
       bola.audio:stop() bola.audio:play()
     end
+
     if bola.posicao.Y < bola.raio then
       bola.velocidade.Y = math.abs(bola.velocidade.Y)
-      bola.velocidade.X=bola.velocidade.X+50
-      bola.velocidade.Y=bola.velocidade.Y+50
+      bola.velocidade.X = bola.velocidade.X
+      bola.velocidade.Y = bola.velocidade.Y
       bola.audio:stop() bola.audio:play()
-    elseif bola.posicao.Y > largura-bola.raio then
+    elseif bola.posicao.Y > larguraJanela-bola.raio then
       bola.velocidade.Y = -math.abs(bola.velocidade.Y)
       bola.audio:stop() bola.audio:play()
     end
-    if bola.posicao.X > P1.x and P1.x+P1.largura>bola.posicao.X and P1.y+P1.altura> bola.posicao.Y-20 and 0>bola.velocidade.Y then
+
+    if bola.posicao.X > Player1.x and Player1.x + Player1.largura > bola.posicao.X and Player1.y + Player1.altura > bola.posicao.Y-20 and 0 > bola.velocidade.Y then
       bola.velocidade.Y = math.abs(bola.velocidade.Y)
-      bola.velocidade.X=bola.velocidade.X+20
-      bola.velocidade.Y=bola.velocidade.Y+20
+      bola.velocidade.X = bola.velocidade.X+50
+      bola.velocidade.Y = bola.velocidade.Y+50
       bola.audio:stop() bola.audio:play()
-    elseif bola.posicao.X > P2.x and P2.x+P2.largura>bola.posicao.X and bola.posicao.Y+20>P2.y+P2.altura and bola.velocidade.Y>0 then 
+    elseif bola.posicao.X > Player2.x and Player2.x + Player2.largura > bola.posicao.X and bola.posicao.Y + 20 > Player2.y + Player2.altura and bola.velocidade.Y > 0 then
       bola.velocidade.Y = -math.abs(bola.velocidade.Y)
-      bola.velocidade.X=bola.velocidade.X-20
-      bola.velocidade.Y=bola.velocidade.Y-20
+      bola.velocidade.X = bola.velocidade.X-50
+      bola.velocidade.Y = bola.velocidade.Y-50
       bola.audio:stop() bola.audio:play()
-    --- Critério para pontuação ---
-    elseif P1.y-P1.altura/2>bola.posicao.Y then
-      bola.posicao.X = comprimento/2
-      bola.posicao.Y = largura/2
-      P1.x=posicaoHorizontal
-      P2.x=posicaoHorizontal
-      ParaJogo=true
-      P2.placar=P2.placar+1
-      bola.velocidade.X=2*defineCos()
-      bola.velocidade.Y=2*defineSen()
-    elseif bola.posicao.Y>P2.y+P2.altura/2 then
-      bola.posicao.X = comprimento/2
-      bola.posicao.Y = largura/2
-      P1.x=posicaoHorizontal
-      P2.x=posicaoHorizontal
-      ParaJogo=true
-      P1.placar=P1.placar+1
-      bola.velocidade.X=2*defineCos()
-      bola.velocidade.Y=2*defineSen()
-  end
+    elseif Player1.y - Player1.altura/2 > bola.posicao.Y then
+      bola.posicao.X = comprimentoJanela/2
+      bola.posicao.Y = larguraJanela/2
+      Player1.x = posicaoHorizontal
+      Player2.x = posicaoHorizontal
+      jogo.encerraPartida = true
+      Player2.placar = Player2.placar+1
+      bola.velocidade.X = 2 * jogo.cosseno()
+      bola.velocidade.Y = 2 * jogo.seno()
+    elseif bola.posicao.Y > Player2.y + Player2.altura/2 then
+      bola.posicao.X = comprimentoJanela/2
+      bola.posicao.Y = larguraJanela/2
+      Player1.x = posicaoHorizontal
+      Player2.x = posicaoHorizontal
+      jogo.encerraPartida = true
+      Player1.placar = Player1.placar + 1
+      bola.velocidade.X = 2 * jogo.cosseno()
+      bola.velocidade.Y = 2 * jogo.seno()
+    end
 end
-function movimentaP1(dt)
-  if love.keyboard.isDown("right") then --- Movimentação do player 1 
-      P1.x=P1.x + 300*dt
-      ParaJogo=false
-      mensagem=true
+
+local function movimentaP1(dt)
+  if love.keyboard.isDown("right") then
+      Player1.x = Player1.x + 300 * dt
+      jogo.encerraPartida = false
+      jogo.mostrarMensagemInicial = false
     elseif love.keyboard.isDown("left") then
-      P1.x=P1.x - 300*dt
-      ParaJogo=false
-      mensagem=true
+      Player1.x = Player1.x - 300 * dt
+      jogo.encerraPartida = false
+      jogo.mostrarMensagemInicial = false
     end
-    if P1.x+P1.largura>comprimento then --- Colisão com o canto da tela
-      P1.x=P1.x-5
-    elseif 0>P1.x then
-      P1.x=P1.x+5
+    if Player1.x + Player1.largura > comprimentoJanela then
+      Player1.x = Player1.x-5
+    elseif 0 > Player1.x then
+      Player1.x = Player1.x+5
     end
 end
-function restart()
-  if love.keyboard.isDown("return") then
-    FinalPartida=false
-    P1.placar=0
-    P2.placar=0
+
+local function movimentaPlayer(velocidadeMovimento, comandoRecebido)
+  if comandoRecebido == ConstanteLove.comandoMoverDireita then
+      Player2.x = Player2.x + 300 * velocidadeMovimento
+      jogo.encerraPartida=false
+      jogo.mostrarMensagemInicial = false
+  elseif comandoRecebido == ConstanteLove.comandoMoverEsquerda then
+      Player2.x = Player2.x - 300 * velocidadeMovimento
+      jogo.encerraPartida = false
+      jogo.mostrarMensagemInicial = false
+  end
+  if Player2.x+Player2.largura > comprimentoJanela then
+    Player2.x=Player2.x-5
+  elseif 0>Player2.x then
+    Player2.x = Player2.x+5
   end
 end
-function love.load()
-  fonte=love.graphics.newFont(ConstanteLove.nomeFonte, 24)
-  --mqttLove.start(ConstanteLove.hostServer, "luca16s", ConstanteLove.canal, trataMensagemRecebida)
-  construirJanela()
+
+local function ObjetosDesenhaveis()
+  love.graphics.setFont(jogo.fonte)
+  if jogo.mostrarMensagemInicial == true then
+    love.graphics.print("O primeiro a marcar 5 pontos vence!", 65, 200)
+  end
+
+  love.graphics.circle("fill", bola.posicao.X, bola.posicao.Y, bola.raio)
+  love.graphics.setColor(1, 0, 0)
+  love.graphics.setColor(1, 1, 1)
+
+  love.graphics.rectangle("fill", Player1.x, Player1.y, Player1.largura, Player1.altura + 5)
+  love.graphics.rectangle("fill", Player2.x, Player2.y, Player2.largura, Player2.altura + 5)
+  love.graphics.print("P1 : " .. Player1.placar, 50, -10 + larguraJanela/2)
+  love.graphics.print("P2 : " .. Player2.placar, comprimentoJanela - 150, -10 + larguraJanela/2)
+
+  if Player1.placar == ConstanteLove.finalJogo  then
+    love.graphics.print("P1 Venceu!", comprimentoJanela - 500, larguraJanela - 450)
+    love.graphics.print("Pressione Enter para jogar novamente!", comprimentoJanela - 765, larguraJanela - 400)
+    jogo.partidaTerminada = true
+  elseif Player2.placar == ConstanteLove.finalJogo then
+    love.graphics.print("P2 Venceu!", comprimentoJanela - 500, larguraJanela - 450)
+    love.graphics.print("Pressione Enter para jogar novamente!", comprimentoJanela - 765, larguraJanela - 400)
+    jogo.partidaTerminada = true
+  end
 end
+
+local function restart()
+  if love.keyboard.isDown("return") then
+    jogo.partidaTerminada = false
+    Player1.placar = 0
+    Player2.placar = 0
+  end
+end
+
+function love.load()
+  construirJanela()
+  MqttServer.start(ConstanteLove.hostServer, 'luca16s', ConstanteLove.canalJogo,  movimentaPlayer)
+end
+
 function love.update(dt)
   movimentaP1(dt)
   restart()
-  if ParaJogo==false and FinalPartida==false then
-    movimentaBola(dt, bola) 
+  if jogo.encerraPartida == false and jogo.partidaTerminada == false then
+    movimentaBola(dt, bola)
+  end
+  MqttServer.checkMessages()
+end
 
-end
-end
-function ObjetosDesenhaveis()
-  love.graphics.setFont(fonte)
-  if mensagem==false then
-    love.graphics.print("O primeiro a marcar 2 pontos vence!", 65, 200)
-  end
-  love.graphics.rectangle("fill", P1.x,P1.y ,P1.largura,P1.altura+5)
-  love.graphics.rectangle("fill", P2.x,P2.y ,P2.largura,P2.altura+5)
-  love.graphics.print("P1 : "..P1.placar,50,-10+largura/2)
-  love.graphics.print("P2 : "..P2.placar,comprimento-150,-10+largura/2)
-  love.graphics.circle("fill", bola.posicao.X, bola.posicao.Y, bola.raio)
-  if P1.placar==2  then
-    love.graphics.print("P1 Venceu!",comprimento-500,largura-450)
-    love.graphics.print("Pressione Enter para jogar novamente!",comprimento-765,largura-400)
-    FinalPartida=true
-  elseif P2.placar==2 then
-    love.graphics.print("P2 Venceu!",comprimento-500,largura-450)
-    love.graphics.print("Pressione Enter para jogar novamente!",comprimento-765,largura-400)
-    FinalPartida=true
-  end
-end
 function love.draw()
   ObjetosDesenhaveis()
 end
