b9500492cd364516378b45a2a3b20d3b7f37dad8
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2020-12-09 08:19:27 -0300

    - Criado arquivo de objetos do jogo

---
+++ PongObjects.lua
@@ -0,0 +1,61 @@
+local ConstanteLove = require 'ConstanteLove'
+PongObjects = {}
+
+PongObjects.posicaoHorizontal = (ConstanteLove.comprimentoJanela / 2) - (ConstanteLove.comprimentoJogador / 2)
+math.randomseed(os.time())
+
+local function defineAngulo()
+  local angulo = math.random(55, 90)
+  local criterio = math.random(0, 9)
+
+  if criterio%2 == 1 then
+    angulo = -angulo
+  end
+  return angulo
+end
+
+PongObjects.Jogo = {
+    fonte = love.graphics.newFont(ConstanteLove.nomeFonte, ConstanteLove.tamanhoFonte),
+    seno = defineAngulo,
+    cosseno = defineAngulo,
+    mostrarMensagemInicial = true,
+    reiniciarJogo = false,
+    iniciarPartida = false,
+    pausarPartida = false
+}
+
+PongObjects.Bola = {
+      --imagem = love.graphics.newImage(ConstanteLove.imagemBola),
+      raio = ConstanteLove.raioBola,
+      posicao = {
+          X = (ConstanteLove.comprimentoJanela / 2) - (ConstanteLove.raioBola / 2),
+          Y = (ConstanteLove.larguraJanela / 2) - (ConstanteLove.raioBola / 2),
+      },
+      velocidade = {
+          X = 10,
+          Y = -200,
+      },
+      audio = love.audio.newSource(ConstanteLove.somBola, 'static'),
+}
+
+PongObjects.Player1 = {
+    --imagem = love.graphics.newImage(ConstanteLove.imagemJogador),
+    X = PongObjects.posicaoHorizontal,
+    Y = 10,
+    largura = ConstanteLove.comprimentoJogador,
+    altura = ConstanteLove.alturaJogador,
+    placar = 0,
+    comando = nil
+}
+
+PongObjects.Player2 = {
+    --imagem = love.graphics.newImage(ConstanteLove.imagemJogador),
+    X = PongObjects.posicaoHorizontal,
+    Y = ConstanteLove.larguraJanela - 20,
+    largura = ConstanteLove.comprimentoJogador,
+    altura = ConstanteLove.alturaJogador,
+    placar = 0,
+    comando = nil
+}
+
+  return PongObjects
--- main.lua
+++ main.lua
@@ -1,76 +1,27 @@
 local ConstanteLove = require 'ConstanteLove'
+local ObjetosPong = require 'PongObjects'
 local MqttServer = require 'mqttLoveLibrary'
-local comprimentoJanela, larguraJanela = love.graphics.getDimensions()
-local posicaoHorizontal = (comprimentoJanela / 2) - (ConstanteLove.comprimentoJogador / 2)
-math.randomseed(os.time())
 
-local function defineAngulo()
-  local angulo = math.random(55, 90)
-  local criterio = math.random(0, 9)
-
-  if criterio%2 == 1 then
-    angulo = -angulo
-  end
-  return angulo
-end
-
-local jogo = {
-  fonte = love.graphics.newFont(ConstanteLove.nomeFonte, ConstanteLove.tamanhoFonte),
-  seno = defineAngulo,
-  cosseno = defineAngulo,
-  mostrarMensagemInicial = true,
-  partidaTerminada = false,
-  encerraPartida = true
-}
-
-local bola = {
-    --imagem = love.graphics.newImage(ConstanteLove.imagemBola),
-    raio = ConstanteLove.raioBola,
-    posicao = {
-        X = (comprimentoJanela / 2) - (ConstanteLove.raioBola / 2),
-        Y = (larguraJanela / 2) - (ConstanteLove.raioBola / 2),
-    },
-    velocidade = {
-        X = 10,
-        Y = -200,
-    },
-    audio = love.audio.newSource(ConstanteLove.somBola, 'static'),
-}
-
-local Player1 = {
-  --imagem = love.graphics.newImage(ConstanteLove.imagemJogador),
-  X = posicaoHorizontal,
-  Y = 10,
-  largura = ConstanteLove.comprimentoJogador,
-  altura = ConstanteLove.alturaJogador,
-  placar = 0,
-  comando = nil
-}
-
-local Player2 = {
-  --imagem = love.graphics.newImage(ConstanteLove.imagemJogador),
-  X = posicaoHorizontal,
-  Y = larguraJanela - 20,
-  largura = ConstanteLove.comprimentoJogador,
-  altura = ConstanteLove.alturaJogador,
-  placar = 0,
-  comando = nil
-}
+local Jogo = ObjetosPong.Jogo
+local Bola = ObjetosPong.Bola
+local Jogador1 = ObjetosPong.Player1
+local Jogador2 = ObjetosPong.Player2
 
 local function construirJanela()
+  love.window.setMode(ConstanteLove.comprimentoJanela, ConstanteLove.larguraJanela)
     love.window.setFullscreen(false)
     love.window.setTitle(ConstanteLove.TituloJogo)
     love.window.setIcon(love.image.newImageData(ConstanteLove.IconeJogo))
 end
 
-local function movimentaBola(velocidade, bola)
+local function movimentaBola(velocidade, bola, jogador1, jogador2)
     bola.posicao.X = bola.posicao.X + bola.velocidade.X * velocidade
     bola.posicao.Y = bola.posicao.Y + bola.velocidade.Y * velocidade
 
     if bola.posicao.X < bola.raio then
       bola.velocidade.X = math.abs(bola.velocidade.X)
       bola.audio:stop() bola.audio:play()
-    elseif bola.posicao.X > comprimentoJanela-bola.raio then
+    elseif bola.posicao.X > ConstanteLove.comprimentoJanela - bola.raio then
       bola.velocidade.X = -math.abs(bola.velocidade.X)
       bola.audio:stop() bola.audio:play()
     end
@@ -80,47 +31,44 @@ local function movimentaBola(velocidade, bola)
       bola.velocidade.X = bola.velocidade.X
       bola.velocidade.Y = bola.velocidade.Y
       bola.audio:stop() bola.audio:play()
-    elseif bola.posicao.Y > larguraJanela-bola.raio then
+    elseif bola.posicao.Y > ConstanteLove.larguraJanela - bola.raio then
       bola.velocidade.Y = -math.abs(bola.velocidade.Y)
       bola.audio:stop() bola.audio:play()
     end
 
-    if bola.posicao.X > Player1.X and Player1.X + Player1.largura > bola.posicao.X and Player1.Y + Player1.altura > bola.posicao.Y-20 and 0 > bola.velocidade.Y then
+    if bola.posicao.X > jogador1.X and jogador1.X + jogador1.largura > bola.posicao.X and jogador1.Y + jogador1.altura > bola.posicao.Y-20 and 0 > bola.velocidade.Y then
       bola.velocidade.Y = math.abs(bola.velocidade.Y)
       bola.velocidade.X = bola.velocidade.X + 50
       bola.velocidade.Y = bola.velocidade.Y + 50
       bola.audio:stop() bola.audio:play()
-    elseif bola.posicao.X > Player2.X and Player2.X + Player2.largura > bola.posicao.X and bola.posicao.Y + 20 > Player2.Y + Player2.altura and bola.velocidade.Y > 0 then
+    elseif bola.posicao.X > jogador2.X and jogador2.X + jogador2.largura > bola.posicao.X and bola.posicao.Y + 20 > jogador2.Y + jogador2.altura and bola.velocidade.Y > 0 then
       bola.velocidade.Y = -math.abs(bola.velocidade.Y)
       bola.velocidade.X = bola.velocidade.X - 50
       bola.velocidade.Y = bola.velocidade.Y - 50
       bola.audio:stop() bola.audio:play()
-    elseif Player1.Y - Player1.altura/2 > bola.posicao.Y then
-      bola.posicao.X = comprimentoJanela/2
-      bola.posicao.Y = larguraJanela/2
-      Player1.X = posicaoHorizontal
-      Player2.X = posicaoHorizontal
-      jogo.encerraPartida = true
-      Player2.placar = Player2.placar + 1
+    elseif jogador1.Y - jogador1.altura/2 > bola.posicao.Y then
+      bola.posicao.X = ConstanteLove.comprimentoJanela/2
+      bola.posicao.Y = ConstanteLove.larguraJanela/2
+      jogador1.X = PongObjects.posicaoHorizontal
+      jogador2.X = PongObjects.posicaoHorizontal
+      jogador2.placar = jogador2.placar + 1
       MqttServer.sendMessage(ConstanteLove.comandoPontoJogador2, ConstanteLove.canalJogo)
-      bola.velocidade.X = 2 * jogo.cosseno()
-      bola.velocidade.Y = 2 * jogo.seno()
-    elseif bola.posicao.Y > Player2.Y + Player2.altura/2 then
-      bola.posicao.X = comprimentoJanela/2
-      bola.posicao.Y = larguraJanela/2
-      Player1.X = posicaoHorizontal
-      Player2.X = posicaoHorizontal
-      jogo.encerraPartida = true
-      Player1.placar = Player1.placar + 1
+      bola.velocidade.X = 2 * Jogo.cosseno()
+      bola.velocidade.Y = 2 * Jogo.seno()
+    elseif bola.posicao.Y > jogador2.Y + jogador2.altura/2 then
+      bola.posicao.X = ConstanteLove.comprimentoJanela/2
+      bola.posicao.Y = ConstanteLove.larguraJanela/2
+      jogador1.X = PongObjects.posicaoHorizontal
+      jogador2.X = PongObjects.posicaoHorizontal
+      jogador1.placar = jogador1.placar + 1
       MqttServer.sendMessage(ConstanteLove.comandoPontoJogador1, ConstanteLove.canalJogo)
-      bola.velocidade.X = 2 * jogo.cosseno()
-      bola.velocidade.Y = 2 * jogo.seno()
+      bola.velocidade.X = 2 * Jogo.cosseno()
+      bola.velocidade.Y = 2 * Jogo.seno()
     end
 end
 
 local function realizaMovimento(jogo, jogador, movimento, velocidade)
   jogador.X = jogador.X + movimento * velocidade
-  jogo.encerraPartida = false
   jogo.mostrarMensagemInicial = false
 end
 
@@ -132,60 +80,63 @@ local function validaColisaoJogador(jogador, comprimentoJanela)
   end
 end
 
-local function movimentaJogador(player, velocidade)
-  if player.comando == ConstanteLove.comandoMoverDireita then
-    realizaMovimento(jogo, player, 300, velocidade)
-  elseif player.comando == ConstanteLove.comandoMoverEsquerda then
-    realizaMovimento(jogo, player, -300, velocidade)
+local function movimentaJogador(jogador, jogo, velocidade)
+  if jogador.comando == ConstanteLove.comandoMoverDireita then
+    realizaMovimento(jogo, jogador, 300, velocidade)
+  elseif jogador.comando == ConstanteLove.comandoMoverEsquerda then
+    realizaMovimento(jogo, jogador, -300, velocidade)
   end
 end
 
-local function movimentaP1(velocidade)
-  movimentaJogador(Player1, velocidade)
-  validaColisaoJogador(Player1, comprimentoJanela)
+local function movimentaP1(jogador, jogo, velocidade)
+  movimentaJogador(jogador, jogo, velocidade)
+  validaColisaoJogador(jogador, ConstanteLove.comprimentoJanela)
 end
 
-local function movimentaP2(velocidade)
-  movimentaJogador(Player2, velocidade)
-  validaColisaoJogador(Player2, comprimentoJanela)
+local function movimentaP2(jogador, jogo, velocidade)
+  movimentaJogador(jogador, jogo, velocidade)
+  validaColisaoJogador(jogador, ConstanteLove.comprimentoJanela)
 end
 
 local function movimentaPlayerNode(comandoRecebido)
-  Player2.comando = comandoRecebido
+  Jogador2.comando = comandoRecebido
 end
 
 local function ObjetosDesenhaveis()
-  love.graphics.setFont(jogo.fonte)
-  if jogo.mostrarMensagemInicial == true then
-    love.graphics.print("O primeiro a marcar 5 pontos vence!", 65, 200)
+  love.graphics.setFont(Jogo.fonte)
+  if Jogo.mostrarMensagemInicial then
+    love.graphics.print(string.format("O primeiro a marcar %d pontos vence!", ConstanteLove.pontuacaoFinalJogo), 65, 200)
   end
 
-  love.graphics.circle("fill", bola.posicao.X, bola.posicao.Y, bola.raio)
+  love.graphics.circle("fill", Bola.posicao.X, Bola.posicao.Y, Bola.raio)
   love.graphics.setColor(1, 0, 0)
   love.graphics.setColor(1, 1, 1)
 
-  love.graphics.rectangle("fill", Player1.X, Player1.Y, Player1.largura, Player1.altura + 5)
-  love.graphics.rectangle("fill", Player2.X, Player2.Y, Player2.largura, Player2.altura + 5)
-  love.graphics.print("P1 : " .. Player1.placar, 50, -10 + larguraJanela/2)
-  love.graphics.print("P2 : " .. Player2.placar, comprimentoJanela - 150, -10 + larguraJanela/2)
+  love.graphics.rectangle("fill", Jogador1.X, Jogador1.Y, Jogador1.largura, Jogador1.altura + 5)
+  love.graphics.rectangle("fill", Jogador2.X, Jogador2.Y, Jogador2.largura, Jogador2.altura + 5)
 
-  if Player1.placar == ConstanteLove.finalJogo  then
-    love.graphics.print("P1 Venceu!", comprimentoJanela - 500, larguraJanela - 450)
-    love.graphics.print("Pressione Enter para jogar novamente!", comprimentoJanela - 765, larguraJanela - 400)
-    jogo.partidaTerminada = true
-  elseif Player2.placar == ConstanteLove.finalJogo then
-    love.graphics.print("P2 Venceu!", comprimentoJanela - 500, larguraJanela - 450)
-    love.graphics.print("Pressione Enter para jogar novamente!", comprimentoJanela - 765, larguraJanela - 400)
-    jogo.partidaTerminada = true
+  love.graphics.print(string.format("P1 : %d", Jogador1.placar), 50, -10 + ConstanteLove.larguraJanela/2)
+  love.graphics.print(string.format("P2 : %d", Jogador2.placar), ConstanteLove.comprimentoJanela - 150, -10 + ConstanteLove.larguraJanela/2)
+
+  if Jogador1.placar == ConstanteLove.pontuacaoFinalJogo  then
+    love.graphics.print("P1 Venceu!", ConstanteLove.comprimentoJanela - 500, ConstanteLove.larguraJanela - 450)
+    love.graphics.print("Pressione Enter para jogar novamente!", ConstanteLove.comprimentoJanela - 765, ConstanteLove.larguraJanela - 400)
+    Jogo.reiniciarJogo = true
+  elseif Jogador2.placar == ConstanteLove.pontuacaoFinalJogo then
+    love.graphics.print("P2 Venceu!", ConstanteLove.comprimentoJanela - 500, ConstanteLove.larguraJanela - 450)
+    love.graphics.print("Pressione Enter para jogar novamente!", ConstanteLove.comprimentoJanela - 765, ConstanteLove.larguraJanela - 400)
+    Jogo.reiniciarJogo = true
   end
 end
 
-local function restart()
-  if love.keyboard.isDown("return") then
-    jogo.partidaTerminada = false
-    Player1.placar = 0
-    Player2.placar = 0
-  end
+local function reiniciarJogo()
+  package.loaded.ObjetosPong = nil
+  ObjetosPong = require 'PongObjects'
+
+  Jogo = ObjetosPong.Jogo
+  Bola = ObjetosPong.Bola
+  Jogador1 = ObjetosPong.Player1
+  Jogador2 = ObjetosPong.Player2
 end
 
 function love.load()
@@ -194,12 +145,18 @@ function love.load()
 end
 
 function love.update(dt)
-  movimentaP1(dt)
-  movimentaP2(dt)
-  restart()
-  if jogo.encerraPartida == false and jogo.partidaTerminada == false then
-    movimentaBola(dt, bola)
+  movimentaP1(Jogador1, Jogo, dt)
+  movimentaP2(Jogador2, Jogo, dt)
+
+  if Jogo.iniciarPartida or Jogo.pausarPartida then
+    movimentaBola(dt, Bola, Jogador1, Jogador2)
   end
+
+  if Jogo.reiniciarJogo == true then
+    reiniciarJogo()
+    movimentaBola(dt, Bola, Jogador1, Jogador2)
+  end
+
   MqttServer.checkMessages()
 end
 
@@ -209,14 +166,27 @@ end
 
 function love.keypressed(key)
     if key == 'left' then
-      Player1.comando = ConstanteLove.comandoMoverEsquerda
+      Jogador1.comando = ConstanteLove.comandoMoverEsquerda
+      Jogo.iniciarPartida = true
     elseif key == 'right' then
-      Player1.comando = ConstanteLove.comandoMoverDireita
+      Jogo.iniciarPartida = true
+      Jogador1.comando = ConstanteLove.comandoMoverDireita
+    elseif key == 'return' then
+      Jogo.reiniciarJogo = true
+    elseif key == 'space' then
+      if Jogo.pausarPartida then
+        Jogo.pausarPartida = false
+      else
+        Jogo.pausarPartida = true
+      end
     end
 end
 
 function love.keyreleased(key)
   if key == 'left' or key == 'right' then
-    Player1.comando = ConstanteLove.comandoParar
+    Jogador1.comando = ConstanteLove.comandoParar
+  end
+  if key == 'return' then
+    Jogo.reiniciarJogo = false
   end
 end
